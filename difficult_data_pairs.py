4d5674f94104cd8e7002eba86dda8e035fb753d3
Jinghao Lei <j38lei@uwaterloo.ca>
2023-03-15 15:22:58 -0400

    updated with difficult_pairs_output

--- difficult_data_pairs.py
+++ difficult_data_pairs.py
@@ -1,16 +1,24 @@
 import os
+from datetime import datetime
+
 import modules.util as util
 
-home=os.path.abspath(os.getcwd())
-data_path=os.path.join(home, 'data_shortlist')
+home = os.path.abspath(os.getcwd())
+data_path = os.path.join(home, 'data_train_valset')  #data_shortlist
 
 #global data
-pairs=[]
-classes=[]
+pairs = []
+classes = []
 
 for file in os.listdir(data_path):
     classes.append(file)
+print("number of classes to process: ", len(classes))
 
+t1 = datetime.now()
 util.make_pairs(data_path, pairs, classes)
-print(pairs)
-print(classes)
+t2 = datetime.now()
+print("time difference: ", t2 - t1)
+for pair in pairs:
+    print(pair[0].split('\\')[-2:], pair[1].split('\\')[-2:])
+    print(pair)
+    print()
