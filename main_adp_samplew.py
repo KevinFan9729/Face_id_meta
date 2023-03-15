4b005d74b8b462fd15c5212a1e16f74e1577bad5
Halim Rat <halim.rat@gmail.com>
2023-03-15 13:19:52 -0400

    fix main_samplesw.py

--- main_adp_samplew.py
+++ main_adp_samplew.py
@@ -61,12 +61,13 @@ for epoch in range(0,epochs):
         opt.learning_rate.assign(lr)
     
     history = siamese_network.fit_generator(train_generator, validation_data=val_generator, epochs=1)
-    shuffled_ytrue = []
     if epoch % 10 == 1:
         pred = siamese_network.predict(train_generator)
+        shuffled_ytrue = []
         for index in train_generator.indexes:
             shuffled_ytrue.append(train_generator.labels[index][2])
-        for i in range(0,len(shuffled_ytrue)):
+        range_num = len(shuffled_ytrue)-(len(shuffled_ytrue)%16)
+        for i in range(0,range_num):
             if shuffled_ytrue[i] == pred[i]:
                 train_generator.labels[train_generator.indexes[i]][-1] = 1
             else:
